5cde452fa9118085348e6f2fab8b6246802ec6ee
Gareth Coles <gdude2002@gmail.com>
2017-08-02 14:31:17 +0100

    Attempt to relay embeds

--- bot/client.py
+++ bot/client.py
@@ -268,7 +268,7 @@ class Client(discord.client.Client):
                                 hook["id"], hook["token"], wait=True,
                                 content=split_line, username=message.author.name,
                                 avatar_url=avatar if avatar else None,
-                                embeds=[embed.to_dict() for embed in message.embeds] if message.embeds else None
+                                embeds=message.embeds
                             )
                 except Exception as e:
                     await self.send_message(
